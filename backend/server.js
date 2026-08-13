caa4017db7bbfb5bf95d63ad788e1bf754711b59
Ishmeet Kaur <the-email-you-use-for-ishmeet200426@gmail.com>
2026-08-13 14:46:41 +0530

    Add password authentication for students

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
 const app = express();
 app.set("etag", false);
